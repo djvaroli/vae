2cdcc5803ad675bc63dd2be2a9609a18f208cf27
Daniel Varoli <daniel.varoli@gmail.com>
2022-02-04 21:20:13 -0500

    [fix]: fix setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 
 dev = [
     "black==22.1.0",
-    "isort==5.10.1"
+    "isort==5.10.1",
+    "pytest==7.0.0"
 ]
 
 setuptools.setup(
@@ -11,8 +12,12 @@ setuptools.setup(
     author="Daniel John Varoli",
     description="Variational Auto Encoder",
     url="https://github.com/djvaroli/vae",
-    package_dir={"": "vae"},
+    package_dir={"": ""},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
-        "tensorflow==2.7.0"
+        "tensorflow>=2.7.0"
     ]
 )
